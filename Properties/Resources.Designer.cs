a98de3004de187f4c2e01559390a0e7d2dc4677f
itwasrealpashayo <itwasrealpashayo@gmail.com>
2024-02-01 13:05:56 +1000

    Edit MainPage and Add Logo and Ico for App

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace PCInventory.Properties {
         }
         
         /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon IcoPCInventory {
+            get {
+                object obj = ResourceManager.GetObject("IcoPCInventory", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap ImgDevice {
